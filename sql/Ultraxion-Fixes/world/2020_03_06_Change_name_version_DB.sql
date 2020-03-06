cb2de437f4e4f1dd4328a168d89025264a035856
Mistix <47088016+Mistixsnet@users.noreply.github.com>
2020-03-06 18:00:44 +0100

    Change Version

---
+++ sql/Ultraxion-Fixes/world/2020_03_06_Change_name_version_DB.sql
@@ -0,0 +1,1 @@
+UPDATE `version` SET `core_version`='0000-00-00 00:00:00 +0000', `core_revision`='Archived', `db_version`='Ultraxion-4.3.3', `cache_id`='49' WHERE (`core_version`='0000-00-00 00:00:00 +0000');
